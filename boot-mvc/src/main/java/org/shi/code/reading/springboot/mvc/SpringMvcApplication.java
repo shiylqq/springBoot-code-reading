ae802d661688d770781eadc694d847f2757748df
sliang <shink2010@gmail.com>
2022-03-23 02:17:45 +0800

    调试springboot

--- boot-mvc/src/main/java/org/shi/code/reading/springboot/mvc/SpringMvcApplication.java
+++ boot-mvc/src/main/java/org/shi/code/reading/springboot/mvc/SpringMvcApplication.java
@@ -2,10 +2,11 @@ package org.shi.code.reading.springboot.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringMvcApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringMvcApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringMvcApplication.class, args);
     }
 }
